2a1422f80ba5c81aeb8257a424c3252a966ab94c
Agent-grey <markkibuthu@gmail.com>
2024-07-20 18:53:54 +0300

    comple lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+
+const mondayWork = function(activity = 'go to the office'){
+    console.log(`On Mondays, ${activity}.`);
+}
+
+
+const wrapAdjective = function(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}

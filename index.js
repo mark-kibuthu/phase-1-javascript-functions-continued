8963ca3a765024feb44317ec42e349d66bff12db
Agent-grey <markkibuthu@gmail.com>
2024-07-24 06:17:24 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-// code your solution here
-function saturdayFun(activity = 'roller-skate'){
-return `This Saturday, I want to ${activity}!`
-}
-saturdayFun()
+// code your solution here//
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
 
-const mondayWork = function(activity = 'go to the office'){
-    console.log(`On Mondays, ${activity}.`);
-}
+  function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
 
-
-const wrapAdjective = function(flair = "*") {
-    return function(adjective = "special") {
-        return `You are ${flair}${adjective}${flair}!`;
-    }
-}
+  function wrapAdjective(flair = '*') {
+    return function(adjective = 'special') {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
